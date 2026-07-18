c2cf7ed28bdf2f9b90318bf84f15738667704ec6
deentechcompany-boop <deentechcompany@gmail.com>
2026-07-18 18:58:11 +0100

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,13 @@
 import { PrismaClient } from "@prisma/client";
-import * as dotenv from "dotenv";
 
-dotenv.config();
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-export const prisma = new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
